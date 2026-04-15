fba5874e4f57f5896c5c03cb238975a4743cb0bc
nayemmmmmmmmmm <nayemarafat786@gmail.com>
2026-04-15 22:11:21 +0600

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,26 @@
+import os
+import shutil
+
+from copystatic import copy_files_recursive
+from gencontent import generate_page, generate_pages_recursive
+
+dir_path_static = "./static"
+dir_path_public = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
+
+def main():
+    print("Deleting public directory...")
+    try:
+        if os.path.exists(dir_path_public):
+            shutil.rmtree(dir_path_public)
+    except PermissionError:
+        print("Could not delete public directory, proceeding...")
+
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
+
+    print("Generating content...")
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
+
+main()
